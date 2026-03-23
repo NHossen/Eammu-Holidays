a6910708af3a296e2af61898df6846ba3400723e
naeem <naeemhossen2509@gmail.com>
2026-03-23 18:03:45 +0400

    api updated

---
+++ src/app/loading.js
@@ -0,0 +1,25 @@
+// app/loading.js
+"use client";
+
+export default function Loading() {
+  return (
+    <div className="flex flex-col items-center justify-center h-screen space-y-4">
+      <div className="text-center py-10 flex flex-col items-center justify-center gap-4">
+        <div className="relative h-28 w-28">
+          {/* Outer spinning ring */}
+          <div className="absolute inset-0 animate-spin rounded-full border-t-2 border-b-2 border-[#ffcc00]"></div>
+
+          {/* Animated Logo */}
+          <img
+            src="/eammu_holidays_logo.webp"
+            alt="Loading..."
+            className="h-full w-full object-contain animate-pulse px-2"
+          />
+        </div>
+        <p className="text-[#005a31] font-bold text-lg animate-bounce">
+          Welcome To Eammu ...
+        </p>
+      </div>
+    </div>
+  );
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,19 @@
-import Image from "next/image";
-import "./globals.css";
+// app/page.js
+"use client";
+
 import HeroSection from "@/Components/HeroSection/HeroSection";
+import { HelmetProvider, Helmet } from "react-helmet-async";
+import "./globals.css";
+
 export default function Home() {
   return (
-    <div >
-     <HeroSection />
-    </div>
+    <HelmetProvider>
+      <Helmet>
+        <title>Eammu Holidays | Home</title>
+        <meta name="description" content="Welcome to Eammu Holidays" />
+      </Helmet>
+
+      <HeroSection />
+    </HelmetProvider>
   );
 }
