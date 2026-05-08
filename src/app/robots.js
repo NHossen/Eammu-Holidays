a7ed19d568e7c3a5c9ea41d606f565bb03c08aad
naeem <naeemhossen2509@gmail.com>
2026-05-08 20:19:00 +0400

    robot updated

--- src/app/robots.js
+++ src/app/robots.js
@@ -1,22 +1,16 @@
 export default function robots() {
   return {
     rules: [
-      // Main rule
       {
         userAgent: "*",
         allow: "/",
-        disallow: ["/api/", "/log-in", "/sign-up"],
+        disallow: ["/api/", "/log-in/", "/sign-up/"],
       },
 
-      // Block common scrapers
       { userAgent: "AhrefsBot", disallow: "/" },
       { userAgent: "SemrushBot", disallow: "/" },
       { userAgent: "MJ12bot", disallow: "/" },
       { userAgent: "DotBot", disallow: "/" },
-
-      // Explicitly allow Google
-      { userAgent: "Googlebot", allow: "/" },
-      { userAgent: "Bingbot", allow: "/" },
     ],
     sitemap: "https://eammu.com/sitemap.xml",
   };
